62a69541912785d892ad88dbe297ee5ed780c7d6
PortalHUN <kszabolcs1020@gmail.com>
2025-07-15 15:21:09 +0200

    Remove custom theme

--- scripts/.local/bin/ph-audio-switcher.py
+++ scripts/.local/bin/ph-audio-switcher.py
@@ -8,20 +8,29 @@ IDs = []
 Titles = []
 chosen = ""
 
+def get_active():
+  return subprocess.run(["pactl","list","sinks"], capture_output=True,text=True).stdout.split('Active Port:')[1].split('Formats:')[0].strip()
+
 def get_sinks():
   return subprocess.run(["pactl","list","sinks"],capture_output=True, text=True).stdout.split('Ports:')[1].split('Active Port:')[0].strip().replace("\t","").split('\n')
 
 def split_into_arrays(string):
+  active = get_active()
   for item in string:
     manipulated = item.split('(')[0].split(':')
     IDs.append(manipulated[0].strip())
-    Titles.append(manipulated[1].strip())
+    if manipulated[0] == active:
+      Titles.append(manipulated[1].strip()+" (Active)")
+    else:
+      Titles.append(manipulated[1].strip())
 
 def input_string():
   return "\n".join(Titles).encode("utf-8")
 
 def launch_rofi():
-  return subprocess.run(["rofi", "-dmenu", "-i", "-theme", ROFI_THEME, "-p", ROFI_TITLE], input=input_string(), capture_output=True)
+  return subprocess.run(["rofi", "-dmenu", "-p", ROFI_TITLE], input=input_string(), capture_output=True)
+
+#return subprocess.run(["rofi", "-dmenu", "-theme", ROFI_THEME, "-p", ROFI_TITLE], input=input_string(), capture_output=True)
 
 
 #Main
